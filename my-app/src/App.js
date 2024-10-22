d1ecb9e5bd0b2c6967dbb11d363bfed6091cceea
Christoph <Christoph120@users.noreply.github.com>
2024-10-22 18:15:17 +0200

    added routing and pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 import logo from './assets/logo.png'; // Tell webpack this JS file uses this image
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Services from './pages/services'
+import Contact from './pages/contact'
 
 function App() {
   return (
     <div className="grid-container">
       <header className="grid-item">
-        <img src={logo} heigth='50px' width='auto' className="grid-item" alt="Logo" />
+        {/* <img src={logo} style={{height:"20%", width:"20%"}} className="grid-item" alt="Logo" /> */}
       </header>
       <nav className="grid-item">
         <ul>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>About</a></li>
-          <li><a href='#'>Services</a></li>
-          <li><a href='#'>Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
       <main className="grid-item">main</main>
       <footer className="grid-item">footer</footer>
     </div>
